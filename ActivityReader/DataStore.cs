6f0cd8b7a86c081389aa87f723ec45089f12378e
Jan-Erik Öhman <janerik.ohman@gmail.com>
2021-03-02 23:29:45 +0100

    Added Q Env var

--- ActivityReader/DataStore.cs
+++ ActivityReader/DataStore.cs
@@ -17,7 +17,8 @@ namespace ActivityReader
         //https://jeo4cyberdemostorage.queue.core.windows.net/activity?sv=2019-02-02&st=2020-01-13T19%3A43%3A00Z&se=2022-01-14T19%3A43%3A00Z&sp=raup&sig=eyzGMrALnBB54V4IEbS9LT%2F331wPrjQtarR2CBk%2F6AM%3D
         public static void Store2(BaseActivity activity)
         {
-            string sasURI = "https://jeo4cyberdemostorage.queue.core.windows.net/activity?sv=2019-02-02&st=2020-01-13T19%3A43%3A00Z&se=2022-01-14T19%3A43%3A00Z&sp=raup&sig=eyzGMrALnBB54V4IEbS9LT%2F331wPrjQtarR2CBk%2F6AM%3D";
+            //string sasURI = "https://jeo4cyberdemostorage.queue.core.windows.net/activity?sv=2019-02-02&st=2020-01-13T19%3A43%3A00Z&se=2022-01-14T19%3A43%3A00Z&sp=raup&sig=eyzGMrALnBB54V4IEbS9LT%2F331wPrjQtarR2CBk%2F6AM%3D";
+            string sasURI = Environment.GetEnvironmentVariable("QEUEU_SAP");
             QueueClient client = new QueueClient(new System.Uri(sasURI));
             var serilizedActivity = JsonSerializer.Serialize(activity);
             var base64String = Convert.ToBase64String(Encoding.UTF8.GetBytes(serilizedActivity));
